70b6f268d1c769d17f1a6405152bc6578094ecd8
ROBERT  ALEDO <robertaledo22@gmail.com>
2020-04-05 23:51:37 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,8 +8,8 @@ class Anagram
     @word = word
   end
 
-  def match(word_array)
-    word_array.select do |word|
+  def match(word_arr)
+    word_arr.select do |word|
       word.split("").sort == @word.split("").sort
     end
   end
